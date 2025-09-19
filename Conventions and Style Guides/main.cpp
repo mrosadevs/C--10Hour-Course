d9368f759d83891152bfb6c5bebb45ee2d016c6d
mrosadevs <giftedlean@gmail.com>
2025-09-19 13:52:52 -0400

    Conventions and Style Guides

---
+++ Conventions and Style Guides/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int slices;
+    std::cout << "Yo fatty how many sliced of pizza do you want";
+    std::cin >> slices;
+    std::cout << "You have " << slices << " slices of pizza." << std::endl;
+}
