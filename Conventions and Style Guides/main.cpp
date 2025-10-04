69dc65404200a9a4ecdbbda6bd3b4979201ae586
mrosadevs <giftedlean@gmail.com>
2025-10-04 13:17:17 -0400

    Updated Conventions and Style Guides

--- Conventions and Style Guides/main.cpp
+++ Conventions and Style Guides/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int slices;
-    std::cout << "Yo fatty how many sliced of pizza do you want";
+    std::cout << "Yo fatty how many sliced of pizza do you want: ";
     std::cin >> slices;
     std::cout << "You have " << slices << " slices of pizza." << std::endl;
 }
