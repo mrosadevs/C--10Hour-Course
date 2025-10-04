f5f8db06f5b95960de76a75e7d5eef8bdab710cd
mrosadevs <giftedlean@gmail.com>
2025-10-04 13:33:10 -0400

    Finished Pow Functions

--- Pow-Functions/main.cpp
+++ Pow-Functions/main.cpp
@@ -3,5 +3,11 @@
 
 int main()
 {
-    std::cout << pow(10, 2);
+    int base, exponent;
+    std::cout << "What is the base exponent?: ";
+    std::cin >> base;
+    std::cout << "What is the exponent?: ";
+    std::cin >> exponent;
+    double power = pow(base, exponent);
+    std::cout << "The power is " << power << std::endl;
 }
